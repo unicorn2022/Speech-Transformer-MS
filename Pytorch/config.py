b4a4d7c3164ec56e741e5697ccbf5960ea394f64
unicorn <496300118@qq.com>
2023-06-29 00:22:08 +0800

    修改了config中数据集的数量

--- Pytorch/config.py
+++ Pytorch/config.py
@@ -25,9 +25,9 @@ checkpoint = None  # path to checkpoint, None if none
 IGNORE_ID = -1
 sos_id = 0
 eos_id = 1
-num_train = 120098
-num_dev = 14326
-num_test = 7176
+num_train = 28539
+num_dev = 2703
+num_test = 2620
 vocab_size = 35663
 
 DATA_DIR = 'data'
